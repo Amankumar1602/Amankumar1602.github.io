7541a985bcaf02f278e489ea910b43487aeb01e6
Harshal Pawshekar <harshalpawshekar999@gmail.com>
2021-07-04 13:01:01 +0530

    Removed window onload event listener.

--- script.js
+++ script.js
@@ -1,39 +1,48 @@
-window.addEventListener("load", () => {
-  const canvas = document.querySelector("#canvas");
-  const ctx = canvas.getContext("2d");
+const canvas = document.querySelector("#canvas");
+const ctx = canvas.getContext("2d");
+let lineWidth = 10
+let lineColor = 'black'
 
-  // canvas sizing 
+// colors for line
+const RED = 'red'
+const BLUE = 'blue'
+const GREEN = 'green'
+const YELLOW = 'yellow'
 
-  canvas.height = window.innerHeight;
-  canvas.width = window.innerWidth;
+// canvas sizing
 
-  // variables
-  let painting = false;
+canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
 
-  function startPosition(){
-    painting = true
-  }
-  function finishedPosition(){
-    painting = false
-    ctx.beginPath();
-  }
-  function draw(e){
-    if(!painting) return;
-    ctx.lineWidth =10;
-    ctx.lineCap = "round";
+// variables
+let painting = false;
 
-    ctx.lineTo(e.clientX, e.clientY );
-    ctx.stroke();
-  }
-  //EventListeners
-  canvas.addEventListener('mousedown',startPosition);
-  canvas.addEventListener('mouseup',finishedPosition);
-  canvas.addEventListener('mousemove', draw);
-});
+function startPosition() {
+	painting = true
+}
+
+function finishedPosition() {
+	painting = false
+	ctx.beginPath();
+}
+
+function draw(e) {
+	if (!painting) return;
+	ctx.lineWidth = lineWidth;
+	ctx.lineCap = "round";
+	ctx.strokeStyle = lineColor
+	ctx.lineTo(e.clientX, e.clientY);
+	ctx.stroke();
+}
+
+//EventListeners
+canvas.addEventListener('mousedown', startPosition);
+canvas.addEventListener('mouseup', finishedPosition);
+canvas.addEventListener('mousemove', draw);
 
 // Resizing when screen length changes 
 
-window.addEventListener('resize' ,()=>{
-  canvas.height = window.innerHeight;
-  canvas.width = window.innerWidth;
+window.addEventListener('resize', () => {
+	canvas.height = window.innerHeight;
+	canvas.width = window.innerWidth;
 });
