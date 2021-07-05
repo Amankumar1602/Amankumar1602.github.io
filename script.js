1c5a8fc87179aac68505116dd6977da8b8378ff2
Harshal Pawshekar <harshalpawshekar999@gmail.com>
2021-07-05 13:15:31 +0530

    Radio btn, background color done and reset.

--- script.js
+++ script.js
@@ -6,27 +6,22 @@ const eraserSize = document.querySelector('#eraser-size')
 const penColor = document.querySelector('#pen-color')
 const resetBtn = document.querySelector('#reset-canvas')
 const saveBtn = document.querySelector('#save-canvas')
+const backgroundColors = document.querySelectorAll('.color-field')
+
+const radioBtns = document.querySelectorAll('input[name="pen-type"]')
 
 
 let lineWidth = 10
+const pencilWidth = 5
 let eraserWidth = 10
 let lineColor = 'black'
-const eraserColor = 'white'
+let eraserColor = 'white'
 
 penSize.value = lineWidth
 eraserSize.value = lineWidth
 penColor.value = 'black'
 
-// colors for line
-const RED = 'red'
-const BLUE = 'blue'
-const GREEN = 'green'
-const YELLOW = 'yellow'
-const BLACK = 'black'
-const WHITE = 'white'
-
 // canvas sizing
-
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
 
@@ -46,8 +41,8 @@ function draw(e) {
 	if (!painting) return;
 	ctx.lineWidth = lineWidth;
 	ctx.lineCap = "round";
-	canvasX = e.pageX - canvas.offsetLeft;
-	canvasY = e.pageY - canvas.offsetTop;
+	let canvasX = e.pageX - canvas.offsetLeft;
+	let canvasY = e.pageY - canvas.offsetTop;
 
 	ctx.strokeStyle = lineColor;
 	ctx.lineTo(canvasX, canvasY);
@@ -72,10 +67,62 @@ const handleEraserWidth = (evt) => {
 	lineWidth = eraserSize.value
 }
 
+const changeTool = (btn) => {
+	switch (btn.value) {
+		case 'pen':
+			lineWidth = penSize.value
+			lineColor = penColor.value
+			eraserWidth = eraserSize.value
+			break
+		case 'pencil':
+			lineWidth = pencilWidth
+			eraserWidth = eraserSize.value
+			lineColor = penColor.value
+			break
+		case 'eraser':
+			lineWidth = pencilWidth
+			eraserWidth = eraserSize.value
+			lineColor = eraserColor
+			break
+	}
+}
+
+const handleRadioInput = () => {
+	for (const radioBtn of radioBtns) {
+		if (radioBtn.checked) {
+			changeTool(radioBtn)
+		}
+	}
+}
+
+const changeBackground = (evt) => {
+	ctx.fillStyle = evt.target.style.backgroundColor
+	ctx.fillRect(0,0, canvas.width, canvas.height)
+	eraserColor = evt.target.style.backgroundColor
+}
+
+const clearScreen = () => {
+	ctx.clearRect(0,0, canvas.width, canvas.height)
+}
+
 // EventListeners to draw a line
 canvas.addEventListener('mousedown', startPosition);
 canvas.addEventListener('mouseup', finishedPosition);
 canvas.addEventListener('mousemove', draw);
+
+// background color
+
+for (const backgroundColor of backgroundColors) {
+	backgroundColor.addEventListener('click', changeBackground)
+}
+
+// radio btn
+for (const btn of radioBtns) {
+	btn.addEventListener('input', handleRadioInput)
+}
+
+// reset button
+resetBtn.addEventListener('click', clearScreen)
 
 // respond to user input
 penSize.addEventListener('input', handleWidthChange)
